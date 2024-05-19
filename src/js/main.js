5347b8cbdc2d22c0e1398f141a11bdf299c31f55
luisaferRP <luisaramiresporras103@gmail.com>
2024-05-19 18:17:36 -0500

    feat: se crea navar y la funcionalidad del togger

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@
 // imports
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
+
+
+// mode dark------------------------------------------------------------
+document.getElementById("toggle-checkbox").addEventListener("change", function () {
+    let toggle = document.getElementById("toggle-checkbox");
+    // color change to the body
+    document.body.classList.toggle("bg-dark");
+});
