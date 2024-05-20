348b752136b687eb482875936c1a43618e2afa80
luisaferRP <luisaramiresporras103@gmail.com>
2024-05-19 19:45:27 -0500

    feat:se crea seccion inicial con imagene y texto

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,9 @@ import * as bootstrap from 'bootstrap'
 
 // mode dark------------------------------------------------------------
 document.getElementById("toggle-checkbox").addEventListener("change", function () {
-    let toggle = document.getElementById("toggle-checkbox");
+    // let toggle = document.getElementById("toggle-checkbox");
+    let navbar = document.getElementById("navbar");
+    navbar.classList.toggle("bg-dark");
     // color change to the body
     document.body.classList.toggle("bg-dark");
 });
