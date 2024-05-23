37eb3afcfad3650a23c5533399eed4b2fba3f68c
luisaferRP <luisaramiresporras103@gmail.com>
2024-05-23 15:22:00 -0500

    feat: se crea masonry para el contenedor de las imagenes,se hacen carpetas para la api y el consumo con url y se crea logica para pintar datos

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,45 @@
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
-let URL = 'http://localhost:3000/tours'
+/* import urls */
+import { URL } from "./api/URLS.js";
+
+/* data that comes from the document */
+let bodyCard = document.querySelector('#contenedor__img')
+
+
+/* funtion find tours */
+async function findTours() {
+    let response = await fetch(URL)
+    let datos = await response.json()
+    paintTours(datos)
+
+    /* return datos */
+}
+
+findTours()
+
+/* paint tours */
+function paintTours(datos) {
+    console.log(datos);
+    for (let i = 0; i < datos.length; i++) {
+
+        bodyCard.innerHTML +=`
+        <div class="col-11 w-100 mt-2 item">
+            <img src="${datos[i].landingImg}" alt="${datos[i].tour}" class="imagen-contenedor rounded-4">
+        </div>
+        `
+    }
+} 
+
+
+
+
+
+
+
+
+
 
 
 // mode dark------------------------------------------------------------
